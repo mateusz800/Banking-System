53688fc960c0c5f3983fe1dc6b8fbf109844f994
Mateusz Budnik <mat.budnik@outlook.com>
2020-11-03 22:07:33 +0100

    connect with db

---
+++ SupportAPI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupportAPI.Data
+{
+    public class DataContext:DbContext
+    {
+        // public DbSet<Account> Accounts { get; set;}
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+    }
+}
